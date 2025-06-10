70a99750d4675389896eed00efba86be3741bb07
Kostiantyn Teimurov <tejmurovkostia111@gmail.com>
2025-06-10 12:54:08 +0200

    sync block with Object monitor

--- src/ait/elevator/task/Truck.java
+++ src/ait/elevator/task/Truck.java
@@ -2,7 +2,10 @@ package ait.elevator.task;
 
 import ait.elevator.model.Elevator;
 
+import java.util.Objects;
+
 public class Truck implements Runnable{
+    private static final Object monitor = new Object();
     private int nRaces;
     private int capacity;
     private Elevator elevator;
@@ -17,7 +20,13 @@ public class Truck implements Runnable{
     public void run() {
 //        synchronized (elevator) {} - так будет работать, но убивает многопоточку. Все ждут пока один тред выполнит работу.
         for (int i = 0; i < nRaces; i++) {
-            synchronized (elevator) {
+//            synchronized (this) - водитель спрашивает сам себя - НЕ работает,
+//            synchronized ("hello") - мы даем любому объекту ключ мутекс - и спрашиваем - она мутекс или нет? - РАБОТАЕТ
+//            synchronized (new String("hello")) - НЕ работает, каждый раз создается новый объект
+//            synchronized (Truck.class) - рефлексия. РАБОТАЕТ как охранник.
+//            synchronized ("mutex") - НЕЛЬЗЯ создавать одинаковые мутекс-ключи. Здесь он отвечает за поезда, там за корабли. Поэтому лучше ссылаться на конкретный объект
+//            synchronized (elevator) - ПРОБЛЕМА: геттеры и сеттеры.
+            synchronized (monitor) {
                 elevator.add(capacity);
             }
         }
